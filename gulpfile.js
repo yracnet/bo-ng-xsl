fed7256a2a97c6ec4d04713a9ae03c0ee0cf8c2c
Willyams Yujra <yracnet@gmail.com>
2017-04-07 08:09:40 -0400

    Suport XSLT compile server

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 //var scss = require('gulp-scss');
 var out = require('gulp-util');
+var xslt = require('gulp-xslt');
 
 var conf = {
 	root: './',
@@ -24,15 +25,23 @@ var conf = {
 	}
 };
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['build-bootstrap'], function () {
+gulp.task('serve', ['build-bootstrap', 'build-xslt'], function () {
 	browserSync.init({
 		startPath: '/',
-		server: './webapp',
+		server: './webapp', 
+		middleware: [
+			function (req, res, next) {
+				if(req.url.indexOf('/views/')>0){
+					req.url = req.url.replace('/views/', '/build-xslt'); 
+				}
+				out.log('req',req.url);
+				next();
+			}
+		],
 		online: false,
 		ui: false
 	});
-	//gulp.watch('./webapp/**/*.{html, js, css, xsl, xslt}').on('change', browserSync.reload);
+
 	gulp.watch([
 		'./webapp/**/*.html',
 		'./webapp/build/*.css',
@@ -41,13 +50,20 @@ gulp.task('serve', ['build-bootstrap'], function () {
 		'./webapp/*.xslt'
 	]).on('change', browserSync.reload);
 	gulp.watch('./modules/bootstrap/3.3.7/stylesheets/**/*.scss', ['build-bootstrap']);
-});
+	gulp.watch('./webapp/views/*.html', ['build-xslt']);
 
+});
+	
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('build-bootstrap', function () {
 	return gulp.src('./modules/bootstrap/3.3.7/stylesheets/bootstrap.scss')
 									.pipe(sass().on('error', sass.logError))
 									.pipe(gulp.dest('./webapp/build'));
+});
+gulp.task('build-xslt', function() {
+    gulp.src('./webapp/views/*.html')
+        .pipe(xslt('./webapp/layout.xsl'))
+        .pipe(gulp.dest('./webapp/build-xslt/'));
 });
 
 
